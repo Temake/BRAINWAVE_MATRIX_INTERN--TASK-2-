2630717f80752635f2f55a9a8bf74a798fa282d9
Temake <130784362+Temake@users.noreply.github.com>
2024-10-26 03:11:48 +0100

    n

--- blog/settings.py
+++ blog/settings.py
@@ -52,7 +52,7 @@ INTERNAL_IPS = [
 ]
 TAILWIND_APP_NAME = 'theme'
 
-NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+NPM_BIN_PATH = "/c/Program Files/nodejs/npm"
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
